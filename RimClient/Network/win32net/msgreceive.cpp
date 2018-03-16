a5fb8662486ae654f44479d6affa519f65ff6cf8
yang <407859345@qq.com>
2018-03-16 12:54:38 +0800

    客户端网络模块分离

---
+++ RimClient/Network/win32net/msgreceive.cpp
@@ -0,0 +1,254 @@
+﻿#include "msgreceive.h"
+
+#include <QUdpSocket>
+#include <QDebug>
+
+#include "Util/rlog.h"
+#include "Util/rutil.h"
+#include "netglobal.h"
+
+#define MSG_RECV_BUFF 1024
+
+namespace ClientNetwork{
+
+RecveiveTask::RecveiveTask(QObject *parent) :
+    tcpSocket(NULL),RTask(parent)
+{
+
+}
+
+void RecveiveTask::setSock(RSocket *sock)
+{
+    tcpSocket = sock;
+}
+
+void RecveiveTask::startMe()
+{
+    RTask::startMe();
+    if(!isRunning())
+    {
+        start();
+    }
+}
+
+void RecveiveTask::stopMe()
+{
+    if(runningFlag)
+    {
+        RTask::stopMe();
+        runningFlag = false;
+    }
+}
+
+void RecveiveTask::run()
+{
+    RLOG_INFO("Start Receive++++++++++++++++++++++");
+
+    runningFlag = true;
+    lastRecvBuff.clear();
+
+    char recvBuff[MSG_RECV_BUFF] = {0};
+
+    while(runningFlag)
+    {
+        memset(recvBuff,0,MSG_RECV_BUFF);
+        int recvLen = tcpSocket->recv(recvBuff,MSG_RECV_BUFF);
+        if(recvLen > 0)
+        {
+            if(lastRecvBuff.size() > 0)
+            {
+                int tmpBuffLen = lastRecvBuff.size() + recvLen + 1;
+                char * dataBuff = new char[tmpBuffLen];
+                memset(dataBuff,0,tmpBuffLen * sizeof(char));
+
+                memcpy(dataBuff,lastRecvBuff.data(),lastRecvBuff.size());
+                memcpy(dataBuff + lastRecvBuff.size(),recvBuff,recvLen);
+
+                recvData(dataBuff,lastRecvBuff.size() + recvLen);
+                lastRecvBuff.clear();
+
+                delete[] dataBuff;
+            }
+            else
+            {
+                recvData(recvBuff,recvLen);
+            }
+        }
+        else
+        {
+            RLOG_ERROR("receive a pack not completely %d",tcpSocket->getLastError());
+            tcpSocket->closeSocket();
+            emit socketError(tcpSocket->getLastError());
+            break;
+        }
+    }
+
+    runningFlag = false;
+    RLOG_INFO("Stop Receive++++++++++++++++++++++");
+}
+
+void RecveiveTask::recvData(char * recvData,int recvLen)
+{
+    DataPacket packet;
+    memset((char *)&packet,0,sizeof(DataPacket));
+
+    if(recvLen > sizeof(DataPacket))
+    {
+        unsigned int processLen = 0;
+        do
+        {
+            memcpy((char *)&packet,recvData,sizeof(DataPacket));
+            processLen += sizeof(DataPacket);
+
+            //[1]数据头部分正常
+            if(packet.magicNum == RECV_MAGIC_NUM)
+            {
+                QByteArray dataBuff;
+                //[1.1]至少存在多余一个完整数据包
+                if(packet.currentLen <= recvLen - processLen)
+                {
+                    //[1.1.1]一包数据
+                    if(packet.totalIndex == 1)
+                    {
+                        dataBuff.resize(packet.currentLen);
+                        memcpy(dataBuff.data(),recvData + processLen,packet.currentLen);
+
+                        processData(dataBuff);
+                    }
+                    //[1.1.2]多包数据(只保存数据部分)
+                    else
+                    {
+                       QByteArray data;
+                       data.resize(packet.currentLen);
+                       memcpy(data.data(),recvData + processLen,packet.currentLen);
+
+                       if(packetBuffs.value(packet.packId,NULL) == NULL)
+                       {
+                            PacketBuff * buff = new PacketBuff;
+                            buff->totalPackIndex = packet.totalIndex;
+                            buff->recvPackIndex += 1;
+                            buff->recvSize += packet.currentLen;
+                            buff->buff.append(data);
+
+                            packetBuffs.insert(packet.packId,buff);
+                       }
+                       else
+                       {
+                            PacketBuff * buff = packetBuffs.value(packet.packId,NULL);
+                            if(buff)
+                            {
+                                buff->buff.append(data);
+                                buff->recvSize += packet.currentLen;
+                                buff->recvPackIndex += 1;
+                                if(buff->recvPackIndex == buff->totalPackIndex)
+                                {
+                                    buff->isCompleted = true;
+
+                                    dataBuff.append(buff->getFullData());
+
+                                    packetBuffs.remove(packet.packId);
+                                    delete buff;
+
+                                    processData(dataBuff);
+                                }
+                            }
+                       }
+                    }
+                    processLen += packet.currentLen;
+
+                    //[1.1.3]
+                    int leftLen = recvLen - processLen;
+
+                    if(leftLen <= 0)
+                    {
+                        break;
+                    }
+
+                    if(leftLen >= sizeof(DataPacket))
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        //[1.1.3.1]【信息被截断】
+                        memcpy(&packet,recvData + processLen,leftLen);
+
+                        lastRecvBuff.clear();
+                        lastRecvBuff.append((char *)&packet,leftLen);
+
+                        processLen += leftLen;
+                        break;
+                    }
+                }
+                //[1.2]【信息被截断】
+                else
+                {
+                    int leftLen = recvLen - processLen;
+
+                    lastRecvBuff.clear();
+                    lastRecvBuff.append((char *)&packet,sizeof(DataPacket));
+                    lastRecvBuff.append(recvData+processLen,leftLen);
+
+                    processLen += leftLen;
+                    break;
+                }
+            }
+            else
+            {
+                qDebug()<<"Recv Error Packet";
+            }
+        }while(processLen <= recvLen);
+    }
+    else
+    {
+        lastRecvBuff.clear();
+        lastRecvBuff.append(recvData,recvLen);
+    }
+}
+
+RecveiveTask::~RecveiveTask()
+{
+
+}
+
+TextReceive::TextReceive(QObject *parent):
+    RecveiveTask(parent)
+{
+
+}
+
+TextReceive::~TextReceive()
+{
+
+}
+
+void TextReceive::processData(QByteArray &data)
+{
+    G_TextRecvMutex.lock();
+    G_TextRecvBuffs.enqueue(data);
+    G_TextRecvMutex.unlock();
+
+    G_TextRecvCondition.wakeOne();
+}
+
+FileReceive::FileReceive(QObject *parent):
+    RecveiveTask(parent)
+{
+
+}
+
+FileReceive::~FileReceive()
+{
+
+}
+
+void FileReceive::processData(QByteArray &data)
+{
+    G_FileRecvMutex.lock();
+    G_FileRecvBuffs.enqueue(data);
+    G_FileRecvMutex.unlock();
+
+    G_FileRecvCondition.wakeOne();
+}
+
+} //namespace ClientNetwork;
---
+++ RimClient/Network/win32net/msgreceive.h
@@ -0,0 +1,79 @@
+﻿/*!
+ *  @brief     文本信息接收
+ *  @details   接收文本信息，接收后直接保存由其它程序处理
+ *  @file      msgreceive.h
+ *  @author    wey
+ *  @version   1.0
+ *  @date      2018.01.06
+ *  @warning
+ *  @copyright NanJing RenGu.
+ *  @note   20180123:wey:调整客户端支持分包接收、组包
+ *          20180305:wey:调整接收结构，RecveiveTask完全封装数据组包接收，子类负责对数据进行处理
+ */
+#ifndef MSGRECEIVE_H
+#define MSGRECEIVE_H
+
+#include <QHash>
+
+#include "../network_global.h"
+#include "../rtask.h"
+#include "../rsocket.h"
+
+namespace ClientNetwork{
+
+class NETWORKSHARED_EXPORT RecveiveTask : public RTask
+{
+    Q_OBJECT
+public:
+    explicit RecveiveTask(QObject *parent = 0);
+    virtual ~RecveiveTask();
+
+    void setSock(RSocket * sock);
+
+    void startMe();
+    void stopMe();
+
+signals:
+    void socketError(int errorCode);
+
+protected:
+    void run();
+    void recvData(char * recvData,int recvLen);
+    virtual void processData(QByteArray & data)=0;
+
+protected:
+    QString errorString;
+
+    RSocket * tcpSocket;
+    QByteArray lastRecvBuff;                        //断包接收缓冲区
+    QHash<int,PacketBuff*> packetBuffs;             //多包缓冲区
+};
+
+class NETWORKSHARED_EXPORT TextReceive : public RecveiveTask
+{
+    Q_OBJECT
+public:
+    explicit TextReceive(QObject *parent = 0);
+    ~TextReceive();
+
+private:
+    void processData(QByteArray & data);
+
+};
+
+class NETWORKSHARED_EXPORT FileReceive : public RecveiveTask
+{
+    Q_OBJECT
+public:
+    explicit FileReceive(QObject * parent = 0);
+    ~FileReceive();
+
+private:
+    void processData(QByteArray & data);
+
+};
+
+
+} //ClientNetwork
+
+#endif // MSGRECEIVE_H
